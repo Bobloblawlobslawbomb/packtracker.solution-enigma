363cbfd4a956074673950965131bece790d77113
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-05-12 12:03:07 -0700

    before lunch commit
    Co-Authored-By:  John Edmondson <edmondsonj@gmail.com>
    Co-Authored-By:  John Edmondson <edmondsonj@gmail.com>

--- PackTracker/Models/PackList.cs
+++ PackTracker/Models/PackList.cs
@@ -12,20 +12,24 @@ namespace PackTracker.Models
       AllLists = new Dictionary<string, List<string>>() { };
     }
 
-    public void AddList(string ListName, List<string> PackingList)
+    // public void AddList(string ListName, List<string> PackingList)
+    // {
+    //   AllLists.Add(ListName, PackingList);
+    // }
+
+    // private static List<>
+
+    public void AddList(object NewPackList)
     {
-      AllLists.Add(ListName, PackingList);
+      AllLists.Add(NewPackList.PackName, NewPackList.ListItems);
+      PackList.ClearAll();
     }
 
     public static List<PackList> GetAll()
     {
-      return _instances;
+      return AllLists;
     }
 
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
 
   }
   public class PackList
@@ -33,13 +37,20 @@ namespace PackTracker.Models
     public string PackName { get; set; }
     private static List<PackList> ListItems = new List<PackList> { };
 
-
     public PackList(string packName)
     {
       PackName = packName;
       ListItems = new List<PackList>();
       ListItems.Add(this);
     }
+    public static void ClearAll()
+    {
+      Packlist.Clear();
+    }
+
+
+
 
   }
+
 }
